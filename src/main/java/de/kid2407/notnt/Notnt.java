9e45fb6b4fbfeb73992c9cb4f9155386540fe840
Tobias Franz <cinex.mail@web.de>
2020-05-02 22:06:18 +0200

    Version 1.0.1
    Signed-off-by: Tobias Franz <cinex.mail@web.de>

--- src/main/java/de/kid2407/notnt/Notnt.java
+++ src/main/java/de/kid2407/notnt/Notnt.java
@@ -10,7 +10,7 @@ public class Notnt {
 
     public static final String MOD_ID = "notnt";
     public static final String MOD_NAME = "Notnt";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public Notnt() {
         MinecraftForge.EVENT_BUS.register(this);
